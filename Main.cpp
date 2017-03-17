ade076a4c47ec8a270f0a2ae07406177d63d22e5
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-03-17 16:55:02 -0600

    Agregar y listar creados.

--- Main.cpp
+++ Main.cpp
@@ -28,16 +28,79 @@ int main() {
 		cout << "4) Salir" << endl;
 		int respuesta1;
 		cin >> respuesta1;
+		while (respuesta1 < 1 || respuesta1 > 4) {
+			cout << "Numero invalido, ingrese su opcion de nuevo!" << endl;
+			cin >> respuesta1;
+		}
 		if (respuesta1 == 1)
 		{
-			cout << "Que tipo de bender sera?" << endl;
+			string nombre;
+			double HP;
+			double ofensa;
+			double defensa;
+			int nivelDS;
+			cout << "Ingrese nombre del bender: " << endl;
+			cin >> nombre;
+			cout << "Ingrese el HP del bender: " << endl;
+			cin >> HP;
+			while (HP < 0) {
+				cout << "HP invalido, ingrese el HP de nuevo!";
+				cin >> HP;
+			}
+			cout << "Ingrese la ofensa del bender: " << endl;
+			cin >> ofensa;
+			while (ofensa < 0 || ofensa > 60) {
+				cout << "Ofensa invalida, ingrese la ofensa de nuevo!";
+				cin >> ofensa;
+			}
+			cout << "Ingrese la defensa del bender: " << endl;
+			cin >> defensa;
+			while (defensa < 0) {
+				cout << "Defensa invalida, ingrese la defensa de nuevo!";
+				cin >> defensa;
+			}
+			cout << "Ingrese el nivel de suerte: " << endl;
+			cin >> nivelDS;
+			while (nivelDS < 0 || nivelDS > 10) {
+				cout << "Nivel de suerte invalido, ingrese el nivel de suerte de nuevo!" << endl;
+				cin >> nivelDS;
+			}
+ 			cout << "Que tipo de bender sera?" << endl;
 			cout << "1) Air" << endl;
 			cout << "2) Earth" << endl;
 			cout << "3) Fire" << endl;
 			cout << "4) Water" << endl;
+			int respuesta2;
+			cin >> respuesta2;
+			while (respuesta2 < 1 || respuesta2 > 4) {
+				cout << "Numero invalido, ingrese su opcion de nuevo!" << endl;
+				cin >> respuesta2;
+			}
+			if (respuesta2 == 1)
+			{
+				listaBenders.push_back(new airbenders(nombre, HP, ofensa, defensa, nivelDS));
+				cout << "Airbender agregado exitosamente!" << endl;
+			} else if (respuesta2 == 2)
+			{
+				listaBenders.push_back(new earthbenders(nombre, HP, ofensa, defensa, nivelDS));
+				cout << "Earthbender agregado exitosamente!" << endl;
+			} else if (respuesta2 == 3)
+			{
+				listaBenders.push_back(new firebenders(nombre, HP, ofensa, defensa, nivelDS));
+				cout << "Firebender agregado exitosamente!" << endl;
+			} else 
+			{
+				listaBenders.push_back(new waterbenders(nombre, HP, ofensa, defensa, nivelDS));
+				cout << "Waterbender agregado exitosamente!" << endl;
+			}
 		} else if (respuesta1 == 2)
 		{
-			/* code */
+			string nombreT;
+			for (int i = 0; i < listaBenders.size(); ++i)
+			{
+				nombreT = listaBenders.at(i) -> getNombre();
+				cout << (i+1) << ") " << nombreT << endl;
+			}
 		} else if (respuesta1 == 3)
 		{
 			/* code */
